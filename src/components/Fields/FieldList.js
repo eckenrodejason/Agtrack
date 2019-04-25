d77cb5e1b2148741a9ad1e5d8bcbae0278bf7b92
Emily Bossiere <emilybossiere@gmail.com>
2019-04-25 11:22:12 -0700

    added table of dummy API json data and some styling to home page

--- src/components/Fields/FieldList.js
+++ src/components/Fields/FieldList.js
@@ -1,51 +1,87 @@
 import React from "react";
 
-{
-  /*THIS IS PLACEHOLDER CODE*/
+//import "./Post.css";
+
+class FieldList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{props.title}</h1>
+        <p className="Author">Author</p>
+      </div>
+    );
+  }
 }
-const FieldList = props => {
-  return (
-    <div>
-      <header className="header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <h1 className="header__title">Fields</h1>
-      </header>
-      Showing all Fields
-    </div>
-  );
-};
 
 export default FieldList;
 
-{
-  /* import React from "react";
-import Axios from "axios";
-import { unlink } from "fs";
+// import React from "react";
+// import axios from "axios";
 
-export default class Fieldlist extends React.Component {
-  state = {
-    jobs: []
-  };
+// //const API_KEY = "wFdJRAHjwzylncYDdwrcKw";
 
-  componentDidMount() {
-    Axios.get(`https://us.agworld.co/user_api/v1/collection_jobs/{id}`).then(
-      res => {
-        console.log(res);
-        this.ListeningStateChangedEvent({ jobs: res.data });
-      }
-    );
-  }
+// class FieldList extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       guideid: []
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-  render() {
-    return (
-      <ul>
-        {this.UNSAFE_componentWillMount.state.jobs.map(job => (
-          <li>{job.name}</li>
-        ))}
-      </ul>
-    );
-  }
-}
+//   handleClick() {
+//     axios
+//       .get("https://www.ifixit.com/api/2.0/wikis/INFO?display=titles")
+//       .then(response => this.setState({ guideid: response.data }));
+//     //console.log("success");
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <header className="header">
+//           {/*<img src={logo} className="App-logo" alt="logo" />*/}
+//           <h1 className="header__title">Fields</h1>
+//         </header>
+//         <button onClick={this.handleClick}>Show Fields</button>
+//         <ul>
+//           <li>{this.state.guideid}</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
-*/
-}
+// export default FieldList;
+
+// {
+//   /* import React from "react";
+// import Axios from "axios";
+// import { unlink } from "fs";
+
+// export default class Fieldlist extends React.Component {
+//   state = {
+//     jobs: []
+//   };
+
+//   componentDidMount() {
+//     Axios.get(`https://us.agworld.co/user_api/v1/collection_jobs/{id}`).then(
+//       res => {
+//         console.log(res);
+//         this.ListeningStateChangedEvent({ jobs: res.data });
+//       }
+//     );
+//   }
+
+//   render() {
+//     return (
+//       <ul>
+//         {this.UNSAFE_componentWillMount.state.jobs.map(job => (
+//           <li>{job.name}</li>
+//         ))}
+//       </ul>
+//     );
+//   }
+// }
+
+// */
+// }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-#!Content-Type: application/vnd.api+json
-Accept: application/vnd.api+json
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
 
-import React from 'react';
+// #!Content-Type: application/vnd.api+json
+// Accept: application / vnd.api + json;
 
-const url = 'https://us.agworld.co';
+// import React from "react";
 
+// const url = "https://us.agworld.co";
